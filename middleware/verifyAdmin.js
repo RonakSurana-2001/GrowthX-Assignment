14e8b7d5da21a939402f225e7bd9372c2a205475
RonakSurana-2001 <ronaksurana2017@gmail.com>
2024-10-12 08:29:05 +0530

    Changed Middleware

--- middleware/verifyAdmin.js
+++ middleware/verifyAdmin.js
@@ -1,5 +1,8 @@
+const LOCAL="http://localhost:3000/"
+const DEPLOY_URL="https://growthx-assignment-oyx1.onrender.com"
+
 async function verifyAdmin(req,res,next){
-    const getAllAdmins=await fetch("http://localhost:3000/admins");
+    const getAllAdmins=await fetch(`${DEPLOY_URL}/admins`);
     const adminsList=await getAllAdmins.json()
     const data=adminsList.adminsData
     for(let index=0;index<data.length;index++){
